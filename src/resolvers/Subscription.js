87ac0f1273c79903f59428935f97494607561a61
Heath Dunlop <heath.dunlop.hd@gmail.com>
2018-11-21 14:52:02 +1300

    ff

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -9,6 +9,18 @@ const newLeave = {
   subscribe: newLeaveSubscribe,
 }
 
+function newTagSubscribe(parent, args, context, info) {
+  return context.db.subscription.tag(
+    { where: { mutation_in: ["CREATED"] } },
+    info
+  )
+}
+
+const newTag = {
+  subscribe: newTagSubscribe,
+}
+
 module.exports = {
   newLeave,
+  newTag,
 }
